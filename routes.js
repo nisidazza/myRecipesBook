5d77493a61bb61c2457a54405eb727bbadd422f1
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-11-03 17:00:50 +1300

    try to override boostrap css - problem still to be fixed

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Welcome to my Page!')
-})
+    res.render('home')
+    })
 
 module.exports = router
