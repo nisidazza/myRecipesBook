fe4dea29ca0f3961f7724d4d8974f2b4a111357d
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-11-18 21:32:28 +1300

    add new ingredient - frontend

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -1,6 +1,6 @@
-function addIngredientToRecipe(event){
+function addIngredientToRecipe(event) {
 
-    var document = event.target.ownerDocument;    
+    var document = event.target.ownerDocument;
 
     var addNewIngredientTemplate = document.getElementById("addNewIngredientTemplate");
     console.log(addNewIngredientTemplate)
@@ -9,4 +9,23 @@ function addIngredientToRecipe(event){
 
     var myTable = document.getElementById("myTable");
     myTable.appendChild(newIngredientsList);
+}
+
+function toggleNewIngredient(event) {
+    var target = event.target;
+    target.toggled = !target.toggled;
+
+    var listBox = target.parentNode.parentNode.querySelector("#ingredient_ids")
+    var textBox = target.parentNode.parentNode.querySelector("#new_ingredients")
+
+    if (target.toggled) {
+        target.innerText = "Select Ingredient";
+        listBox.style.display = "none";
+        textBox.style.display = "inline";
+    } else {
+        target.innerText = "New Ingredient"
+        listBox.style.display = "inline";
+        textBox.style.display = "none";
+        textBox.value = "";
+    }
 }
